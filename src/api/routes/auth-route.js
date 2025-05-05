7b308ee5af9b77ab0b440ce7e1bdedc2257fb443
SRedRider <shanejehansteelman@gmail.com>
2025-05-05 18:30:41 +0300

    feat: restrict access to user retrieval route to admin users only

--- src/api/routes/auth-route.js
+++ src/api/routes/auth-route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.get('/verify', authController.verifyEmail);
-router.get('/users', authController.getAllUsers);
+router.get('/users', isAdmin, authController.getAllUsers);
 // Forgot password route
 router.post('/forgot-password', authController.forgotPassword);
 // Reset password route
